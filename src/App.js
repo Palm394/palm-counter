bf6c81869f6c1921cd8e6b6be8afa209cf50daaf
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2021-06-04 23:14:08 +0700

    change title & admin setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import { AppBar, Container, Typography } from '@material-ui/core'
 import React from 'react'
 import Player from './component/Player.js'
+import { BrowserRouter as Router ,Switch , Route } from 'react-router-dom'
+import Admin from './component/Admin.js'
 
 function App() {
   return (
     <>
-        <AppBar position='relative' color='primary'>
-          <Typography variant='h3' align='center'>Palm - Counter</Typography>
-        </AppBar>
+        <Router>
+            <AppBar position='relative' color='primary'>
+              <Typography variant='h3' align='center'>Palm - Counter</Typography>
+            </AppBar>
+            <Switch>
+                <Route exact path='/'>
+                  <Container style={{display:'flex',flexWrap:'wrap'}}>
+                    <Player name="Arsonist" />
+                    <Player name="Catalyst" />
+                    <Player name="Inhibitor" />
+                    <Player name="Detective" />
+                    <Player name="(Another) ByStander" />
+                    <Player name="Palm" />
+                  </Container>
+                </Route>
+                <Route path='/admin'>
+                  <Admin />
+                </Route>
+            </Switch>
+            
 
-        <Container style={{display:'flex',flexWrap:'wrap'}}>
-          <Player name="Arsonist" />
-          <Player name="Catalyst" />
-          <Player name="Inhibitor" />
-          <Player name="Detective" />
-          <Player name="(Another) ByStander" />
-          <Player name="Palm" />
-        </Container>
+          
+        </Router>
+          
           
     </>
   );
