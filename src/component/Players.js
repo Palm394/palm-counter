25c42f0b8dce49beada99eb2ac3e57bee76843c6
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2021-08-24 19:16:24 +0700

    Make Route for each season

---
+++ src/component/Players.js
@@ -0,0 +1,27 @@
+import React, {useEffect , useState} from 'react'
+import Player from './Player.js'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+
+const Players = () => {
+
+    const { ss } = useParams()
+    const [score, setScore] = useState([]) 
+
+    useEffect(() => {
+        async function callAPI(){
+            console.log('call score')
+            const result = await axios(`${process.env.REACT_APP_API}/score?season=${ss === undefined ? process.env.REACT_APP_LAST_SS : ss}`)
+            setScore(result.data)
+          }
+        callAPI()
+    },[ss])
+
+    return (
+        <>
+            {Object.entries(score).map(([key,value]) => <Player key={key} name={key} pts={value}/>)}
+        </>
+    )
+}
+
+export default Players
--- src/component/index.js
+++ src/component/index.js
@@ -1,10 +1,10 @@
 import Header from "./Header"
-import Player from "./Player"
+import Players from "./Players"
 import Developer from "./Developer"
 import LastScore from "./LastScore"
 import SideBar from "./SideBar"
 import SideBarFake from "./SideBarFake"
 
 export {
-    Header,Player,Developer,LastScore,SideBar,SideBarFake
+    Header,Players,Developer,LastScore,SideBar,SideBarFake
 }
